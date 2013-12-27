360e85e46884e6cb14ac52f1b8349b1482ee2828
Sammidysam <sammidysam@gmail.com>
2013-12-27 12:38:02 -0500

    Add cjr_get_stem function, documentation I will probably need to check for verb_length being 0, as that could cause problems.

--- src/stem.c
+++ src/stem.c
@@ -9,7 +9,7 @@
 // #include <stdbool.h>
 
 /*
- * Retrieves the ending of the string verb.
+ * Retrieves the ending of string verb.
  * If it has a valid ending, the function will return that ending.
  * If the ending is not valid, the function will return "".
  *
@@ -21,7 +21,21 @@ char *cjr_get_ending(char *verb)
 	return verb;
 }
 
+/*
+ * Retrieves the stem of string verb.
+ * The ending, retrieved from cjr_get_ending,
+ * will be removed from the end of verb.
+ */
 char *cjr_get_stem(char *verb)
 {
+	size_t verb_length = strlen(verb);
+	size_t chars_to_delete = strlen(cjr_get_ending(verb));
+
+	/* TODO: split into own function? */
+	/* Remove last chars_to_delete characters from verb. */
+	for (size_t i = verb_length; i >= verb_length - chars_to_delete; i--) {
+		verb[i] = '\0';
+	}
+	
 	return verb;
 }
