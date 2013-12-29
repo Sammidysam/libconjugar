2bbf779ed206eb5da6e23627b8316437dd9eafcc
Sammidysam <sammidysam@gmail.com>
2013-12-28 23:49:59 -0500

    Write cjr_get_ending function, adjust return value of cjr_get_stem I really need to test these functions.

--- src/stem.c
+++ src/stem.c
@@ -1,5 +1,6 @@
 #include "stem.h"
 
+#include <stdlib.h>
 #include <string.h>
 
 #include "verb.h"
@@ -18,7 +19,30 @@
  */
 char *cjr_get_ending(char *verb)
 {
-	return verb;
+	/* Max verb ending length is 4, so 8 is safe. */
+	char *ending = malloc(8 * sizeof(char));
+	char *last_two_characters = &verb[(strlen(verb) > 2) ? (strlen(verb) - 2) : 0];
+
+	/* If ending is se, then we need to get more information. */
+	if (strcmp(last_two_characters, "se") == 0) {
+		/*
+		 * We need the last four characters to determine
+		 * what kind of verb ending in "se" it is.
+		 */
+		char *last_four_characters = &verb[(strlen(verb) > 4) ? (strlen(verb) - 4) : 0];
+
+		if (strcmp(last_four_characters, "arse") == 0
+		    || strcmp(last_four_characters, "erse") == 0
+		    || strcmp(last_four_characters, "irse") == 0)
+			ending = last_four_characters;
+	} else {
+		if (strcmp(last_two_characters, "ar") == 0
+		    || strcmp(last_two_characters, "er") == 0
+		    || strcmp(last_two_characters, "ir") == 0)
+			ending = last_two_characters;
+	}
+	
+	return strlen(ending) == 0 ? "" : ending;
 }
 
 /*
@@ -37,5 +61,5 @@ char *cjr_get_stem(char *verb)
 		verb[i] = '\0';
 	}
 	
-	return verb;
+	return chars_to_delete > 0 ? verb : "";
 }
