25ff5b55c4f54983eaa9e876263d4a841549b558
Sammidysam <sammidysam@gmail.com>
2014-02-14 23:12:10 -0500

    Make tests for verb ending consistent for stem
    So some tests were added.

--- test/test_verb.c
+++ test/test_verb.c
@@ -19,11 +19,45 @@ START_TEST(test_verb_ending_actual_verbs)
 }
 END_TEST
 
+START_TEST(test_verb_ending_not_verbs)
+{
+	ck_assert_str_eq(cjr_get_ending("these"), "");
+	ck_assert_str_eq(cjr_get_ending("are"), "");
+	ck_assert_str_eq(cjr_get_ending("not"), "");
+	ck_assert_str_eq(cjr_get_ending("verbs"), "");
+}
+END_TEST
+
+START_TEST(test_verb_ending_short_non_verbs)
+{
+	ck_assert_str_eq(cjr_get_ending("a"), "");
+	ck_assert_str_eq(cjr_get_ending("an"), "");
+	ck_assert_str_eq(cjr_get_ending("the"), "");
+	ck_assert_str_eq(cjr_get_ending("that"), "");
+}
+END_TEST
+
+START_TEST(test_verb_ending_short_verbs)
+{
+	ck_assert_str_eq(cjr_get_ending("ser"), "er");
+	ck_assert_str_eq(cjr_get_ending("ver"), "er");
+}
+END_TEST
+
+START_TEST(test_verb_ending_ir)
+{
+	ck_assert_str_eq(cjr_get_ending("ir"), "");
+}
+END_TEST
+
 TCase *verb_case(void)
 {
 	TCase *verb = tcase_create("Verb");
 
 	tcase_add_test(verb, test_verb_ending_actual_verbs);
+	tcase_add_test(verb, test_verb_ending_not_verbs);
+	tcase_add_test(verb, test_verb_ending_short_non_verbs);
+	tcase_add_test(verb, test_verb_ending_short_verbs);
 
 	return verb;
 }
